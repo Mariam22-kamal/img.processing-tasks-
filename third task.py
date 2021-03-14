8b51916e9d4683ed19ef81b923eb9e58eed92e42
Mariam22-kamal <60687586+Mariam22-kamal@users.noreply.github.com>
2021-03-14 21:50:37 +0200

    Update and rename third task to third task.py

--- third task.py
+++ third task.py
@@ -7,8 +7,8 @@ _, frame= cap.read()
 # finds edges in the input image image and  marks them in the output map edges 
 edges = cv2.Canny(frame,100,200)  
 # Display edges in a frame 
-  cv2.imshow('Edges',edges) 
-  # Wait for Esc key to stop 
-  k = cv2.waitKey(5) & 0xFF
-    if k == 27: 
-        break
+cv2.imshow('Edges',edges) 
+# Wait for Esc key to stop 
+k = cv2.waitKey(5) & 0xFF
+  if k == 27: 
+      break
